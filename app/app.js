3ba5fc18052714ed15909c567576993e0cf520e9
Toby Foord <toby@idodev.co.uk>
2016-01-20 23:56:56 +0000

    Refactoring & Reorganising

--- app/app.js
+++ app/app.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var controllers = require('./controllers/index');
 
 var app = express();
 
@@ -23,8 +21,7 @@ app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, '../public')));
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', controllers);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ app/controllers/index.js
@@ -0,0 +1,6 @@
+﻿var router = require('express').Router();
+
+router.use('/', require('./home'));
+router.use('/users', require('./users'));
+
+module.exports = router;
